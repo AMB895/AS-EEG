ff32fa3145e6ec2a0cba12c8c060e42639312a31
Abigail Beatty <amb895@pitt.edu>
2025-03-06 15:10:14 -0500

    RM and time

--- run_permutations.bash
+++ run_permutations.bash
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-#SBATCH --partition=RM-shared
+#SBATCH --partition=RM
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=128
 
-#SBATCH --time 00:17:00
+#SBATCH --time 17:00:00
 echo "[$(date)] STARTING JOB"
 module load matlab
 export NUMBER_OF_PROCESSORS=128
