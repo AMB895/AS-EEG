1e658119f39e2abc7b7b66e1ec871815e726c961
Abigail Beatty <amb895@pitt.edu>
2025-03-07 11:33:27 -0500

    directory to code/

--- run_permutations.bash
+++ run_permutations.bash
@@ -8,6 +8,6 @@
 echo "[$(date)] STARTING JOB"
 module load matlab
 export NUMBER_OF_PROCESSORS=128
-cd /ocean/projects/soc230004p/shared/antisaccade_eeg/
+cd /ocean/projects/soc230004p/shared/antisaccade_eeg/code/
 matlab -nodisplay -r "try,run('permute_tfce'),catch e, e, end; quit"
 echo "[$(date)] STOP JOB"
