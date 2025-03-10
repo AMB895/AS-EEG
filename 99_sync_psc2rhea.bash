df9301da9a6c309bb48d9e18748afa0ba1a800ac
Abigail Beatty <amb895@pitt.edu>
2025-03-10 19:09:49 -0400

    sync test perm tfce files to rhea

--- 99_sync_psc2rhea.bash
+++ 99_sync_psc2rhea.bash
@@ -4,4 +4,4 @@
 #
 # 20250228WF - init
 #
-rsync -ihvaz -n abeatty@bridges2.psc.edu:~/antisaccade_eeg/[p]*.mat /Volumes/Hera/Abby/AS_EEG/TFCE/ 
+rsync -ihvaz abeatty@bridges2.psc.edu:~/antisaccade_eeg/[p]*.mat /Volumes/Hera/Abby/AS_EEG/TFCE/ 
