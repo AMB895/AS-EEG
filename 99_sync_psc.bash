6269ba7a59303636319f1582d57dfc26fa79435b
Abigail Beatty <amb895@pitt.edu>
2025-03-10 19:06:46 -0400

    psc 2 rhea

--- 99_sync_psc.bash
+++ 99_sync_psc.bash
@@ -4,7 +4,7 @@
 #
 # 20250228WF - init
 #
-rsync -ihvaz  PrepPeriodAnalysis/CorrectTrials/[lt]*.mat PrepPeriodAnalysis/*.mat abeatty@bridges2.psc.edu:~/antisaccade_eeg/data/
-rsync -ihvaz  /Volumes/Hera/Abby/AS_EEG/limo_tools/ /Volumes/Hera/Projects/7TBrainMech/scripts/fieldtrip-20180926/ /Volumes/Hera/Abby/Resources/eeglab_current/eeglab2024.2/ abeatty@bridges2.psc.edu:~/antisaccade_eeg/tools/
+rsync -ihvaz PrepPeriodAnalysis/CorrectTrials/[lt]*.mat PrepPeriodAnalysis/*.mat abeatty@bridges2.psc.edu:~/antisaccade_eeg/data/
+rsync -ihvaz /Volumes/Hera/Abby/AS_EEG/limo_tools/ /Volumes/Hera/Projects/7TBrainMech/scripts/fieldtrip-20180926/ /Volumes/Hera/Abby/Resources/eeglab_current/eeglab2024.2/ abeatty@bridges2.psc.edu:~/antisaccade_eeg/tools/
 rsync -ihvaz /Volumes/Hera/Abby/AS_EEG/STUDY/logicalchanneighbmatrix.mat abeatty@bridges2.psc.edu:~/antisaccade_eeg/
 
