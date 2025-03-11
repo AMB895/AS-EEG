ae34604413742d09fa1d0d2f0f53d33301434ee1
Abigail Beatty <amb895@pitt.edu>
2025-03-11 12:51:26 -0400

    track permutation number

--- permute_tfce.m
+++ permute_tfce.m
@@ -61,19 +61,19 @@ end
 % Load t/F values [chans, freqs, times]
 COR_TTEST = load(fullfile(cortrialsdir,'ttest_outputs_cor.mat'),'tval');
 cor_tval = COR_TTEST.tval;
-COR_AGE = load(fullfile(cortrialsdir,'linregress_age_outputs_cor.mat'),'fval');
-cor_fval_age = COR_AGE.fval;
+% COR_AGE = load(fullfile(cortrialsdir,'linregress_age_outputs_cor.mat'),'fval');
+% cor_fval_age = COR_AGE.fval;
 COR_AGEINV = load(fullfile(cortrialsdir,'linregress_invage_outputs_cor.mat'),'fval_inv');
 cor_fval_ageinv = COR_AGEINV.fval_inv;
-COR_INCOR_TTEST = load(fullfile(maindir,'two_sample_ttest_cor_incor.mat'),'t');
-cor_incor_tval = COR_INCOR_TTEST.t;
-COR_ERRCOR_TTEST = load(fullfile(maindir,'two_sample_ttest_cor_errcor.mat'),'t');
-cor_errcor_tval = COR_ERRCOR_TTEST.t;
+% COR_INCOR_TTEST = load(fullfile(maindir,'two_sample_ttest_cor_incor.mat'),'t');
+% cor_incor_tval = COR_INCOR_TTEST.t;
+% COR_ERRCOR_TTEST = load(fullfile(maindir,'two_sample_ttest_cor_errcor.mat'),'t');
+% cor_errcor_tval = COR_ERRCOR_TTEST.t;
 
 % Names of files to be created
 cor_permtfcename = 'permutationTFCE_correct.mat';
-cor_incor_permtfcename = 'permutationTFCE_cor_incor.mat';
-cor_errcor_permtfcename = 'permutationTFCE_cor_errcor.mat';
+% cor_incor_permtfcename = 'permutationTFCE_cor_incor.mat';
+% cor_errcor_permtfcename = 'permutationTFCE_cor_errcor.mat';
 
 % Number of permutations
 nperm = 1000;
@@ -83,38 +83,39 @@ nchans = size(cor_tval,1);
 
 % preallocating max permuted tfce score vectors for speed
 cor_max_perm_tfce_group = zeros(nchans,nperm);
-cor_max_perm_tfce_age = zeros(nchans,nperm);
+% cor_max_perm_tfce_age = zeros(nchans,nperm);
 cor_max_perm_tfce_ageinv = zeros(nchans,nperm);
-cor_incor_max_perm_tfce_group = zeros(nchans,nperm);
-cor_errcor_max_perm_tfce_group = zeros(nchans,nperm);
+% cor_incor_max_perm_tfce_group = zeros(nchans,nperm);
+% cor_errcor_max_perm_tfce_group = zeros(nchans,nperm);
 
 for n=1:nperm
-    %% Correct Trials
+    disp(n)
+    tic
     % getting total size of t-values matrix
     cor_totalSize = size(cor_tval,1)*size(cor_tval,2)*size(cor_tval,3);
-    cor_incor_totalSize = size(cor_incor_tval,1)*size(cor_incor_tval,2)*size(cor_incor_tval,3);
-    cor_errcor_totalSize = size(cor_errcor_tval,1)*size(cor_errcor_tval,2)*size(cor_errcor_tval,3);
+    % cor_incor_totalSize = size(cor_incor_tval,1)*size(cor_incor_tval,2)*size(cor_incor_tval,3);
+    % cor_errcor_totalSize = size(cor_errcor_tval,1)*size(cor_errcor_tval,2)*size(cor_errcor_tval,3);
 
     % reshaping tfce scores to a 1 x totalSize vector
     cor_reshaped_t = reshape(cor_tval,[1,cor_totalSize]);
-    cor_reshaped_f = reshape(cor_fval_age,[1, cor_totalSize]);
+    % cor_reshaped_f = reshape(cor_fval_age,[1, cor_totalSize]);
     cor_reshaped_finv = reshape(cor_fval_ageinv,[1,cor_totalSize]);
-    cor_incor_reshaped_t = reshape(cor_incor_tval,[1,cor_incor_totalSize]);
-    cor_errcor_reshaped_t = reshape(cor_errcor_tval,[1,cor_errcor_totalSize]);
+    % cor_incor_reshaped_t = reshape(cor_incor_tval,[1,cor_incor_totalSize]);
+    % cor_errcor_reshaped_t = reshape(cor_errcor_tval,[1,cor_errcor_totalSize]);
     
     % Permute t-value matrix
     cor_perm_t = cor_reshaped_t(randperm(length(cor_reshaped_t)));
-    cor_perm_f = cor_reshaped_f(randperm(length(cor_reshaped_f)));
+    % cor_perm_f = cor_reshaped_f(randperm(length(cor_reshaped_f)));
     cor_perm_finv = cor_reshaped_finv(randperm(length(cor_reshaped_finv)));
-    cor_incor_perm_t = cor_incor_reshaped_t(randperm(length(cor_incor_reshaped_t)));
-    cor_errcor_perm_t = cor_errcor_reshaped_t(randperm(length(cor_errcor_reshaped_t)));
+    % cor_incor_perm_t = cor_incor_reshaped_t(randperm(length(cor_incor_reshaped_t)));
+    % cor_errcor_perm_t = cor_errcor_reshaped_t(randperm(length(cor_errcor_reshaped_t)));
 
     % Reshape permuted t-values back to original size
     cor_perm_t = reshape(cor_perm_t,[size(cor_tval,1) size(cor_tval,2) size(cor_tval,3)]);
-    cor_perm_f = reshape(cor_perm_f,[size(cor_tval,1) size(cor_tval,2) size(cor_tval,3)]);
+    % cor_perm_f = reshape(cor_perm_f,[size(cor_tval,1) size(cor_tval,2) size(cor_tval,3)]);
     cor_perm_finv = reshape(cor_perm_finv, [size(cor_tval,1) size(cor_tval,2) size(cor_tval,3)]);
-    cor_incor_perm_t = reshape(cor_incor_perm_t,[size(cor_incor_tval,1) size(cor_incor_tval,2) size(cor_incor_tval,3)]);
-    cor_errcor_perm_t = reshape(cor_errcor_perm_t,[size(cor_errcor_tval,1) size(cor_errcor_tval,2) size(cor_errcor_tval,3)]);
+    % cor_incor_perm_t = reshape(cor_incor_perm_t,[size(cor_incor_tval,1) size(cor_incor_tval,2) size(cor_incor_tval,3)]);
+    % cor_errcor_perm_t = reshape(cor_errcor_perm_t,[size(cor_errcor_tval,1) size(cor_errcor_tval,2) size(cor_errcor_tval,3)]);
 
     % updating waitbar only when running on rhea
     if strcmp(hostname(1:end-1),'rhea.wpic.upmc.edu')
@@ -130,19 +131,21 @@ for n=1:nperm
     %               0 = do not show progress bar
     %               1 = show progress bar
     cor_perm_tfce_score_group = limo_tfce(3,cor_perm_t,channeighbmatrix,0);
-    cor_perm_tfce_score_age = limo_tfce(3,cor_perm_f,channeighbmatrix,0);
+    % cor_perm_tfce_score_age = limo_tfce(3,cor_perm_f,channeighbmatrix,0);
     cor_perm_tfce_score_ageinv = limo_tfce(3,cor_perm_finv,channeighbmatrix,0);
-    cor_incor_perm_tfce_score_group = limo_tfce(3,cor_incor_perm_t,channeighbmatrix,0);
-    cor_errcor_perm_tfce_score_group = limo_tfce(3,cor_errcor_perm_t,channeighbmatrix,0);
+    % cor_incor_perm_tfce_score_group = limo_tfce(3,cor_incor_perm_t,channeighbmatrix,0);
+    % cor_errcor_perm_tfce_score_group = limo_tfce(3,cor_errcor_perm_t,channeighbmatrix,0);
 
     % Save maximum TFCE score over all channels
     cor_max_perm_tfce_group(:,n) = max(cor_perm_tfce_score_group,[],[2 3]);
-    cor_max_perm_tfce_age(:,n) = max(cor_perm_tfce_score_age,[],[2,3]);
+    % cor_max_perm_tfce_age(:,n) = max(cor_perm_tfce_score_age,[],[2,3]);
     cor_max_perm_tfce_ageinv(:,n) = max(cor_perm_tfce_score_ageinv,[],[2 3]);
-    cor_incor_max_perm_tfce_group(:,n) = max(cor_incor_perm_tfce_score_group,[],[2 3]);
-    cor_errcor_max_perm_tfce_group(:,n) = max(cor_errcor_perm_tfce_score_group,[],[2 3]);
+    toc;
+    % cor_incor_max_perm_tfce_group(:,n) = max(cor_incor_perm_tfce_score_group,[],[2 3]);
+    % cor_errcor_max_perm_tfce_group(:,n) = max(cor_errcor_perm_tfce_score_group,[],[2 3]);
 end
-save(fullfile(tfcepath,cor_permtfcename),'cor_max_perm_tfce_group','cor_max_perm_tfce_age','cor_max_perm_tfce_ageinv')
-save(fullfile(tfcepath,cor_incor_permtfcename),'cor_incor_max_perm_tfce_group')
-save(fullfile(tfcepath,cor_errcor_permtfcename),'cor_errcor_max_perm_tfce_group')
+save(fullfile(tfcepath,cor_permtfcename),'cor_max_perm_tfce_ageinv','cor_max_perm_tfce_group')
+% save(fullfile(tfcepath,cor_permtfcename),'cor_max_perm_tfce_group','cor_max_perm_tfce_age','cor_max_perm_tfce_ageinv')
+% save(fullfile(tfcepath,cor_incor_permtfcename),'cor_incor_max_perm_tfce_group')
+% save(fullfile(tfcepath,cor_errcor_permtfcename),'cor_errcor_max_perm_tfce_group')
 
