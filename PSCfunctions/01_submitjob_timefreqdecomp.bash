ff191d923d16b8615e0f5234073c593f6ccc732b
Abigail Beatty <amb895@pitt.edu>
2025-07-10 13:47:32 -0400

    directory for bash script

--- PSCfunctions/01_submitjob_timefreqdecomp.bash
+++ PSCfunctions/01_submitjob_timefreqdecomp.bash
@@ -1,2 +1,2 @@
 mkdir log/ -p
-sbatch -J ersp_timefreq_decomp -o log/ersp_timefreq_o-%j.log -e log/ersp_timefreq_e-%j.log run_timefreq_decomp.bash
+sbatch -J ersp_timefreq_decomp -o log/ersp_timefreq_o-%j.log -e log/ersp_timefreq_e-%j.log PSCfunctions/run_timefreq_decomp.bash
