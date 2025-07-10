53f3342e404d58cff540f501e5231dd835fd9720
Abigail Beatty <amb895@pitt.edu>
2025-07-10 09:47:02 -0400

    'more updates'

--- erspFunctions/calc_ersp_invageeffects.m
+++ erspFunctions/calc_ersp_invageeffects.m
@@ -1,23 +1,32 @@
-function [b_age,t,p] = calc_ersp_invageeffects(T,erspdata,numTimes,numFreqs)
+function [b,t] = calc_ersp_invageeffects(T,erspdata,numTimes,numFreqs)
+% erspdata is (subjects,freqs,times)
 % setting up waitbar
 f = waitbar(0,'Starting','Name','Inverse Age Effects');
 for i = 1:numFreqs
-    for j = 1:numTimes
         % updating waitbar
         waitbar(i/numFreqs,f,sprintf('%d out of %d frequency points',i,numFreqs))
-        
+    for j = 1:numTimes
+         % time,freq point data across subjects
         data = squeeze(erspdata(:,i,j));
-        T.power = data;
-        lme = fitlme(T,'power ~ invage + (1 | id)');
+        T.ersp = data;
+        lme = fitlme(T,'ersp ~ 1 + invage + (1 | id)');
         coeftable = lme.Coefficients;
         agecoeftable = coeftable(2,:);
+        intercepttable = coeftable(1,:);
         % Coefficient Tables with rows: 
 % Name, Estimate, SE, tStat, DF, pValue, Lower, Upper
         % Flip sign of age coefficient because input was inverse age
         b_age(i,j) = -1*double(agecoeftable(1,2));
-        t(i,j) = -1*double(agecoeftable(1,4));
-        p(i,j) = double(agecoeftable(1,6));
+        b_intercept(i,j) = double(intercepttable(1,2));
+        t_age(i,j) = -1*double(agecoeftable(1,4));
+        t_intercept(i,j) = double(intercepttable(1,4));
     end
 end
+% add b values to struct
+b.age = b_age;
+b.intercept = b_intercept;
+% add t values to struct 
+t.age = t_age;
+t.intercept = t_intercept;
 close(f)
 end
--- erspFunctions/calc_ersp_linearmixedmodel.m
+++ erspFunctions/calc_ersp_linearmixedmodel.m
@@ -1,4 +1,4 @@
-function [b,t,p] = calc_ersp_linearmixedmodel(T,erspdata1,erspdata2,numTimes,numFreqs)
+function [b,t] = calc_ersp_linearmixedmodel(T,erspdata1,erspdata2,numTimes,numFreqs)
 % setting up waitbar
 f = waitbar(0,'Starting','Name','Linear Mixed Model');
 for i = 1:numFreqs
@@ -6,8 +6,10 @@ for i = 1:numFreqs
     for j = 1:numTimes
         data1 = squeeze(erspdata1(:,i,j));
         data2 = squeeze(erspdata2(:,i,j));
-        T.power = [data1;data2];
-        lme = fitlme(T,'power ~ invage*trialtype + (1 | id)');
+        T.ersp = [data1;data2];
+        lme = fitlme(T,'ersp ~ invage*trialtype + (1 | id)');
+        % can include people that have one or the other and not do weird
+        % matching thing
         coeftable = lme.Coefficients;
         interceptcoeftable = coeftable(1,:);
         agecoeftable = coeftable(2,:);
@@ -26,11 +28,6 @@ for i = 1:numFreqs
         t_trialtype(i,j) = double(trialtypecoeftable(1,4));
         t_interaction(i,j) = double(interactioncoeftable(1,4));
         t_intercept(i,j) = double(interceptcoeftable(1,4));
-
-        p_age(i,j) = double(agecoeftable(1,6));
-        p_trialtype(i,j) = double(trialtypecoeftable(1,6));
-        p_interaction(i,j) = double(interactioncoeftable(1,6));
-        p_intercept(i,j) = double(interceptcoeftable(1,6));
     end
 end
 % saving coefficient values to single structure
@@ -43,10 +40,5 @@ t.age = t_age;
 t.trialtype = t_trialtype;
 t.interaction = t_interaction;
 t.intercept = t_intercept;
-% saving p values to single structure
-p.age = p_age;
-p.trialtype = p_trialtype;
-p.interaction = p_interaction;
-p.intercept = p_intercept;
 close(f)
 end
