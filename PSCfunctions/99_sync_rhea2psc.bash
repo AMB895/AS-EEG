81cbf71b849999971aab9bc62e3980e32ec915be
Abigail Beatty <amb895@pitt.edu>
2025-07-11 09:57:34 -0400

    stim onset data

--- PSCfunctions/99_sync_rhea2psc.bash
+++ PSCfunctions/99_sync_rhea2psc.bash
@@ -3,14 +3,18 @@
 # sync to supercomputer
 #
 # 20250228WF - init
-# rsync EEG data for correct AS and error AS trials
-rsync -ihvaz /Volumes/Hera/Abby/preprocessed_data/anti/AfterWhole/epochclean_homogenize/*cor.{set,fdt} abeatty@bridges2.psc.edu:~/antisaccade_eeg/preprocessed_data/ 
+# rsync EEG data for correct AS and error AS trials (prep and stim onset)
+# rsync -ihvaz /Volumes/Hera/Abby/preprocessed_data/anti/AfterWhole/epochclean_homogenize/*cor.{set,fdt} abeatty@bridges2.psc.edu:~/antisaccade_eeg/preprocessed_data/prep/
+
+rsync -ihvaz /Volumes/Hera/Abby/preprocessed_data/anti/AfterWhole/epochclean_homogenize_stimonset/*cor.{set,fdt} abeatty@bridges2.psc.edu:~/antisaccade_eeg/preprocessed_data/stim/
 
 # rsync EEG data for VGS trials
-rsync -ihvaz /Volumes/Hera/Abby/preprocessed_data/vgs/AfterWhole/epochclean_homogenize/*kept.{set,fdt} abeatty@bridges2.psc.edu:~/antisaccade_eeg/preprocessed_data/
+# rsync -ihvaz /Volumes/Hera/Abby/preprocessed_data/vgs/AfterWhole/epochclean_homogenize/*kept.{set,fdt} abeatty@bridges2.psc.edu:~/antisaccade_eeg/preprocessed_data/prep/
+
+rsync -ihvaz /Volumes/Hera/Abby/preprocessed_data/vgs/AfterWhole/epochclean_homogenize_stimonset/*kept.{set,fdt} abeatty@bridges2.psc.edu:~/antisaccade_eeg/preprocessed_data/stim/
 
 # rsync calc_ersp_timefreq.m to functions
-rsync -ihvaz /Volumes/Hera/Abby/AS_EEG/erspFunctions/calc_ersp_timefreq.m abeatty@bridges2.psc.edu:~/antisaccade_eeg/functions/
+# rsync -ihvaz /Volumes/Hera/Abby/AS_EEG/erspFunctions/calc_ersp_timefreq.m abeatty@bridges2.psc.edu:~/antisaccade_eeg/functions/
 
 # rsync merge7t_eeg.csv and template channels to PSC
-rsync -ihvaz /Volumes/Hera/Abby/AS_EEG/merge7t_eeg.csv /Volumes/Hera/Abby/AS_EEG/templatechannellabels.mat abeatty@bridges2.psc.edu:~/antisaccade_eeg/
+# rsync -ihvaz /Volumes/Hera/Abby/AS_EEG/merge7t_eeg.csv /Volumes/Hera/Abby/AS_EEG/templatechannellabels.mat abeatty@bridges2.psc.edu:~/antisaccade_eeg/
