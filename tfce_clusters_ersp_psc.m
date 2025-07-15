f9f50c00d4aa0bc41092e34244d7ac0e073644a2
Abigail Beatty <amb895@pitt.edu>
2025-07-15 12:27:47 -0400

    do not need functions folder on psc

--- tfce_clusters_ersp_psc.m
+++ tfce_clusters_ersp_psc.m
@@ -2,7 +2,6 @@
 clear; close all
 % adding necessary paths
 addpath('/ocean/projects/soc230004p/shared/antisaccade_eeg/tools/')
-addpath('/ocean/projects/soc230004p/shared/antisaccade_eeg/functions/')
 addpath('/ocean/projects/soc230004p/shared/antisaccade_eeg/data/')
 addpath('/ocean/projects/soc230004p/shared/antisaccade_eeg/data/ClusterStats')
 %% Load in correct, vgs, and error data
