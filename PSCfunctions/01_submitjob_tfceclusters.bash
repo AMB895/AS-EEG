d9e7d5f29d71a36119d41598af3d686ba020cb67
Abigail Beatty <amb895@pitt.edu>
2025-07-19 13:19:55 -0400

    remove date

--- PSCfunctions/01_submitjob_tfceclusters.bash
+++ PSCfunctions/01_submitjob_tfceclusters.bash
@@ -1,3 +1,2 @@
 mkdir log/ -p
-currentdate = $(date + %Y%m%d)
-sbatch -J tfce_ersp_clusters -o log/tfce_ersp_clusters_o_${currentdate}-%j.log -e log/tfce_ersp_clusters_e_${currentdate}-%j.log PSCfunctions/run_tfceERSPclusters.bash
+sbatch -J tfce_ersp_clusters -o log/tfce_ersp_clusters_o-%j.log -e log/tfce_ersp_clusters_e-%j.log PSCfunctions/run_tfceERSPclusters.bash
